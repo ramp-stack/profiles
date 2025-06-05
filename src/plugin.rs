afa44217eddbc902c10bbee69861253f0ba7ddb1
CalebCouch <calebblackcat@gmail.com>
2025-06-05 13:20:41 -0400

    test

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,13 +1,14 @@
 use pelican_ui::{Context, Plugin};
 use maverick_os::air::AirService;
+use maverick_os::runtime;
 pub use crate::service::ProfileRequest;
 
-pub struct ProfilePlugin;
+pub struct ProfilePlugin(runtime::Context);
 impl Plugin for ProfilePlugin {
-    fn new(_ctx: &mut Context) -> Self {ProfilePlugin}
+    fn new(ctx: &mut Context) -> Self {ProfilePlugin(ctx.runtime.clone())}
 }
 impl ProfilePlugin {
-    pub fn request(&self, ctx: &mut Context, request: ProfileRequest) {
-        ctx.runtime.send::<AirService>(serde_json::to_string(&request).unwrap())
+    pub fn request(&mut self, request: ProfileRequest) {
+        self.0.send::<AirService>(serde_json::to_string(&request).unwrap())
     }
 }
