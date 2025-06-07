7f08ba9d6e4029f5cf5ee7a3991aa4cfdf7dfa11
Ella Mae <ella@orange.me>
2025-06-07 15:59:35 -0400

    Wip

--- src/components/avatar.rs
+++ src/components/avatar.rs
@@ -3,6 +3,10 @@ use pelican_ui::Context;
 use pelican_ui::ImageOrientation;
 use base64::{engine::general_purpose, Engine};
 
+use crate::service::Profiles;
+use maverick_os::air::air;
+use air::orange_name::OrangeName;
+
 use pelican_ui_std::{
     Avatar,
     AvatarContent,
@@ -20,23 +24,36 @@ impl AvatarProfiles {
         )
     }
 
-    pub fn new_with_block(ctx: &mut Context, avatar_content: AvatarContent) -> Avatar {
-        Avatar::new(ctx, avatar_content, Some(("block", AvatarIconStyle::Danger)), false, 96.0, None)
+    pub fn new_with_block(ctx: &mut Context, orange_name: &OrangeName) -> Avatar {
+        let content = AvatarContentProfiles::from_orange_name(ctx, orange_name);
+        Avatar::new(ctx, content, Some(("block", AvatarIconStyle::Danger)), false, 96.0, None)
     }
 
-    pub fn new_with_unblock(ctx: &mut Context, avatar_content: AvatarContent) -> Avatar {
-        Avatar::new(ctx, avatar_content, Some(("block", AvatarIconStyle::Danger)), false, 96.0, None)
+    pub fn new_with_unblock(ctx: &mut Context, orange_name: &OrangeName) -> Avatar {
+        let content = AvatarContentProfiles::from_orange_name(ctx, orange_name);
+        Avatar::new(ctx, content, Some(("block", AvatarIconStyle::Danger)), false, 96.0, None)
+    }
+
+    pub fn user(ctx: &mut Context, orange_name: &OrangeName) -> Avatar {
+        let content = AvatarContentProfiles::from_orange_name(ctx, orange_name);
+        Avatar::new(ctx, content, None, false, 128.0, None)
     }
 }
 
 pub struct AvatarContentProfiles;
 impl AvatarContentProfiles {
-    pub fn new_from_encoded(ctx: &mut Context, bytes: &String) -> AvatarContent {
-        // get profile from state and if bytes are none use default here
-        let png_bytes = general_purpose::STANDARD.decode(bytes).unwrap();
-        let image = image::load_from_memory(&png_bytes).unwrap();
-        let image = ctx.assets.add_image(image.into());
-        AvatarContent::Image(image)
+    pub fn from_orange_name(ctx: &mut Context, orange_name: &OrangeName) -> AvatarContent {
+        let profiles = ctx.state().get::<Profiles>();
+        let profile = profiles.0.get(&orange_name).unwrap();
+        match profile.get("avatar") {
+            None => AvatarContentProfiles::default(ctx),
+            Some(bytes) => {
+                let png_bytes = general_purpose::STANDARD.decode(bytes).unwrap();
+                let image = image::load_from_memory(&png_bytes).unwrap();
+                let image = ctx.assets.add_image(image.into());
+                AvatarContent::Image(image)
+            },
+        }
     }
 
     pub fn default(ctx: &mut Context) -> AvatarContent {
--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,17 @@ pub mod service;
 pub mod plugin;
 pub mod events;
 
-#[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
-pub struct Profile {
-    pub user_name: String,
-    pub biography: String,
-    pub identifier: String, // orange identity
-    pub blocked_dids: Vec<String>,
-    // Bitcoin Wallet Associated???
-}
+use maverick_os::air::air;
+pub use air::orange_name::OrangeName;
+
+// #[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
+// pub struct Profile {
+//     pub user_name: String,
+//     pub biography: String,
+//     pub identifier: String, // orange identity
+//     pub blocked_dids: Vec<String>,
+//     // Bitcoin Wallet Associated???
+// }
 
 use sha2::{Digest, Sha256};
 use std::fs;
