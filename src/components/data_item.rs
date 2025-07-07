df0576526405cafd570180747a3d6118ff5d21ac
Ella Mae <ella@orange.me>
2025-07-07 17:12:01 -0400

    wip

--- src/components/data_item.rs
+++ src/components/data_item.rs
@@ -10,7 +10,7 @@ impl DataItemProfiles {
         let orange = orange_name.to_string();
         let orange_name = orange.strip_prefix("orange_name:").unwrap_or(orange.as_str()).to_string();
         let copy = orange_name.clone();
-        let copy_button = Button::secondary(ctx, Some("copy"), "Copy", None, move |ctx: &mut Context| ctx.hardware.copy(copy.clone()));
+        let copy_button = Button::secondary(ctx, Some("copy"), "Copy", None, move |ctx: &mut Context| ctx.hardware.copy(copy.clone()), Some("Copied".to_string()));
         DataItem::new(ctx, None, "Orange Name", Some(&orange_name), None, None, Some(vec![copy_button]))
     }
 
@@ -22,7 +22,7 @@ impl DataItemProfiles {
 
     pub fn address_item(ctx: &mut Context, address: &str) -> DataItem {
         let copy = address.to_string();
-        let copy_button = Button::secondary(ctx, Some("copy"), "Copy", None, move |ctx: &mut Context| ctx.hardware.copy(copy.clone()));
+        let copy_button = Button::secondary(ctx, Some("copy"), "Copy", None, move |ctx: &mut Context| ctx.hardware.copy(copy.clone()), Some("Copied".to_string()));
         DataItem::new(ctx, None, "Bitcoin address", Some(address), None, None, Some(vec![copy_button]))
     }
 }
